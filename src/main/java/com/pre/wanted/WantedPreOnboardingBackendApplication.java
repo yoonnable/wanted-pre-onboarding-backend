84080e4945171adb3212f6663a698d73133d392a
yoonnable <yoonnable@gmail.com>
2023-10-05 23:36:56 +0900

    project init

---
+++ src/main/java/com/pre/wanted/WantedPreOnboardingBackendApplication.java
@@ -0,0 +1,11 @@
+package com.pre.wanted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreOnboardingBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WantedPreOnboardingBackendApplication.class, args);
+    }
+}
