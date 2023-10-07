7115519b51bdc4098a9a183dcf19d4b28af2339e
yoonnable <yoonnable@gmail.com>
2023-10-08 01:26:19 +0900

    test: 채용공고 등록 테스트 코드 구현

---
+++ src/test/java/com/pre/wanted/jobNotice/controller/JobNoticeControllerTest.java
@@ -0,0 +1,79 @@
+package com.pre.wanted.jobNotice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pre.wanted.jobNotice.dto.AddJobNoticeRequest;
+import com.pre.wanted.jobNotice.entity.JobNotice;
+import com.pre.wanted.jobNotice.repository.JobNoticeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class JobNoticeControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    JobNoticeRepository jobNoticeRepository;
+
+    @BeforeEach
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        jobNoticeRepository.deleteAll();
+    }
+
+    @DisplayName("addJobNotice: 채용공고 등록에 성공한다.")
+    @Test
+    public void addJobNotice() throws Exception {
+        // given
+        final String url = "/api/jobNotice";
+        final Long companyId = 1L;
+        final String position = "백엔드 주니어 개발자";
+        final int reward = 1000000;
+        final String contents = "원티드랩에서 백엔드 주니어 개발자를 채용합니다. 자격요건은..";
+        final String skill = "Python";
+        final AddJobNoticeRequest jobNoticeRequest = new AddJobNoticeRequest(companyId, position, reward, contents, skill);
+
+        final String requestBody = objectMapper.writeValueAsString(jobNoticeRequest);
+
+        // when: 설정한 내용을 바탕으로 요청 전송
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        // then
+        result.andExpect(status().isCreated());
+
+        List<JobNotice> jobNotices = jobNoticeRepository.findAll();
+
+        assertThat(jobNotices.size()).isEqualTo(1);
+        assertThat(jobNotices.get(0).getCompany().getId()).isEqualTo(companyId);
+        assertThat(jobNotices.get(0).getPosition()).isEqualTo(position);
+        assertThat(jobNotices.get(0).getReward()).isEqualTo(reward);
+        assertThat(jobNotices.get(0).getContents()).isEqualTo(contents);
+        assertThat(jobNotices.get(0).getSkill()).isEqualTo(skill);
+    }
+}
